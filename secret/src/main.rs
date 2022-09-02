0d46fb3c1247fb1be8b8149fbff9490f1bb9d30c
potemkinViktor <viktorapotemkin@gmail.com>
2022-09-03 00:50:07 +0300

    .

--- secret/src/main.rs
+++ secret/src/main.rs
@@ -1,3 +1,52 @@
+#[derive(Debug)]
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::{Red, Yellow};
+
+#[derive(Debug)]
+enum Traffic {
+    R,
+    Y,
+    G,
+}
+
+use Traffic::*;
+
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {
+                println!("nested_modules");
+            }
+        }
+    }
+}
+
+use a::series::of;
+
+use a::series::of::nested_modules;
+
 fn main() {
-    println!("Hello, world!");
+    of::nested_modules();
+    nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
+
+    println!("{:?}", red);
+    println!("{:?}", yellow);
+    println!("{:?}", green);
+
+    let r = R;
+    let y = Y;
+    let g = G;
+
+    println!("{:?}", r);
+    println!("{:?}", y);
+    println!("{:?}", g);
 }
