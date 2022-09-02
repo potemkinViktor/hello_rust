c4110d73e4bb20531a061f5081a520da6ad8a824
potemkinViktor <viktorapotemkin@gmail.com>
2022-09-03 00:29:31 +0300

    .

---
+++ communicator/target/debug/deps/communicator-01c24c0aa76ffd88.d
@@ -0,0 +1,5 @@
+/Users/viktorpotemkin/Documents/Rust/hello_rust/communicator/target/debug/deps/communicator-01c24c0aa76ffd88: src/main.rs
+
+/Users/viktorpotemkin/Documents/Rust/hello_rust/communicator/target/debug/deps/communicator-01c24c0aa76ffd88.d: src/main.rs
+
+src/main.rs:
---
+++ communicator/target/debug/deps/communicator-2d07fa1061379543.d
@@ -0,0 +1,10 @@
+/Users/viktorpotemkin/Documents/Rust/hello_rust/communicator/target/debug/deps/communicator-2d07fa1061379543.rmeta: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+/Users/viktorpotemkin/Documents/Rust/hello_rust/communicator/target/debug/deps/libcommunicator-2d07fa1061379543.rlib: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+/Users/viktorpotemkin/Documents/Rust/hello_rust/communicator/target/debug/deps/communicator-2d07fa1061379543.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+src/lib.rs:
+src/client.rs:
+src/network/mod.rs:
+src/network/server.rs:
